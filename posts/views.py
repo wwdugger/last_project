69b79f8c534f5535fe2d3b5813b00de266dfb589
bret <patterson.bret@ymail.com>
2022-11-16 16:30:10 -0800

    4th

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from re import template
+from django.views.generic import PostListView, DetailView
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = "Post"
+
+
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = "Post"
+
+
+class PostCreateView(CreateView):
+    template_name = "posts/new.html"
+    model = "Post"
+    fields = ["title", "subtitle", "body"]
